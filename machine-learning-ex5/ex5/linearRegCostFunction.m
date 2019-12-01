59d9a04779967a32a6cad9e6941908b77e08157b
sid42 <s34bhatt@uwaterloo.ca>
2019-12-01 16:05:08 +0530

    assignment 6

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -19,16 +19,16 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+% Regularized cost function
+J = (1/(2*m)) * sum(((theta' * X')' - y).^2) + (lambda/(2*m)) * sum(theta(2:end).^2);
 
-
-
-
-
-
-
-
-
-
+% Regularized gradient 
+for i = 1:size(theta),
+    if i != 1,
+        grad(i) = (1/m) * sum(((theta' * X')' - y).*X(:, i)) + (lambda/m) * theta(i); 
+    else,
+        grad(i) = (1/m) * sum(((theta' * X')' - y).*X(:, i));
+end;
 
 % =========================================================================
 
